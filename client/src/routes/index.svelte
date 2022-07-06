6167b764d3975531850ef678f6d2dfc34803c276
Daniel Yatali <danielyatali902@gmail.com>
2022-07-06 00:40:55 -0400

    fixed card components text size

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -34,13 +34,13 @@
 		<div class="mt-5">
 			<div class="sm:px-6 2xl:container">
 				<div class="grid gap-6 xl:grid-cols-3 md:grid-cols-2">
-					<div class="flex justify-between p-4 items-center rounded-lg border">
+					<div class="flex justify-between p-6 items-center rounded-xl  bg-white">
 						<div class="card-body w-full">
 							<div>
-								<p class="">Number Hours Worked</p>
+								<p class="text-lg font-medium text-gray-700">Number of Hours Worked</p>
 								<div class="flex mt-2 justify-between">
-									<h3>36</h3>
-									<div class="flex gap-2">
+									<h3 class="text-2xl font-bold">36</h3>
+									<div class="flex gap-2 text-green-600">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
 											xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -49,7 +49,6 @@
 											height="17"
 											stroke="currentColor"
 											fill="currentColor"
-											class="text-green-600"
 										>
 											><path
 												d="M2.03,56.52c-2.66,2.58-2.72,6.83-0.13,9.49c2.58,2.66,6.83,2.72,9.49,0.13l27.65-26.98l23.12,22.31 c2.67,2.57,6.92,2.49,9.49-0.18l37.77-38.22v19.27c0,3.72,3.01,6.73,6.73,6.73s6.73-3.01,6.73-6.73V6.71h-0.02 c0-1.74-0.67-3.47-2-4.78c-1.41-1.39-3.29-2.03-5.13-1.91H82.4c-3.72,0-6.73,3.01-6.73,6.73c0,3.72,3.01,6.73,6.73,6.73h17.63 L66.7,47.2L43.67,24.97c-2.6-2.5-6.73-2.51-9.33,0.03L2.03,56.52L2.03,56.52z"
@@ -63,13 +62,13 @@
 						</div>
 					</div>
 
-					<div class="flex justify-between p-4 items-center rounded-lg border">
+					<div class="flex justify-between p-6 items-center rounded-xl  bg-white">
 						<div class="card-body w-full">
 							<div>
-								<p class="">Tasks Completed</p>
+								<p class="text-lg font-medium text-gray-700">Task Completed</p>
 								<div class="flex mt-2 justify-between">
-									<h3>36</h3>
-									<div class="flex gap-2">
+									<h3 class="text-2xl font-bold">36</h3>
+									<div class="flex gap-2 text-red-600">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
 											xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -78,7 +77,6 @@
 											height="17"
 											stroke="currentColor"
 											fill="currentColor"
-											class="text-red-600"
 										>
 											><path
 												d="M2.03,11.52C-0.63,8.94-0.68,4.69,1.9,2.03c2.58-2.66,6.83-2.72,9.49-0.13l27.65,26.98L62.16,6.57 c2.67-2.57,6.92-2.49,9.49,0.18l37.77,38.22V25.7c0-3.72,3.01-6.73,6.73-6.73s6.73,3.01,6.73,6.73v35.63h-0.02 c0,1.74-0.67,3.47-2,4.78c-1.41,1.39-3.29,2.03-5.13,1.91H82.4c-3.72,0-6.73-3.01-6.73-6.73c0-3.72,3.01-6.73,6.73-6.73h17.63 L66.7,20.84L43.67,43.07c-2.6,2.5-6.73,2.51-9.33-0.03L2.03,11.52L2.03,11.52z"
@@ -91,13 +89,13 @@
 							</div>
 						</div>
 					</div>
-					<div class="flex justify-between p-4 items-center rounded-lg border">
+					<div class="flex justify-between p-6 items-center rounded-xl  bg-white">
 						<div class="card-body w-full">
 							<div>
-								<p class="">Rank</p>
+								<p class="text-lg font-medium text-gray-700">Rank</p>
 								<div class="flex mt-2 justify-between">
-									<h3>7th</h3>
-									<div class="flex gap-2">
+									<h3 class="text-2xl font-bold">7th</h3>
+									<div class="flex gap-2 text-red-600">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
 											xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -106,7 +104,6 @@
 											height="17"
 											stroke="currentColor"
 											fill="currentColor"
-											class="text-red-600"
 										>
 											><path
 												d="M2.03,11.52C-0.63,8.94-0.68,4.69,1.9,2.03c2.58-2.66,6.83-2.72,9.49-0.13l27.65,26.98L62.16,6.57 c2.67-2.57,6.92-2.49,9.49,0.18l37.77,38.22V25.7c0-3.72,3.01-6.73,6.73-6.73s6.73,3.01,6.73,6.73v35.63h-0.02 c0,1.74-0.67,3.47-2,4.78c-1.41,1.39-3.29,2.03-5.13,1.91H82.4c-3.72,0-6.73-3.01-6.73-6.73c0-3.72,3.01-6.73,6.73-6.73h17.63 L66.7,20.84L43.67,43.07c-2.6,2.5-6.73,2.51-9.33-0.03L2.03,11.52L2.03,11.52z"
